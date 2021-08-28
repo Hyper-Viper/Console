aed8e050a74fa381754258d3083121b1341c5710
Hyper-Viper <71551888+Hyper-Viper@users.noreply.github.com>
2021-08-28 16:52:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("My first Console")
+
+first_name = "Aditya"
+second_name = "Semwal"
+full_name = first_name.concat(second_name)
+console.log(full_name)
+
+first_num = 6
+second_num = 12
+add = second_num + first_num
+console.log(add)
+sub = second_num - first_num
+console.log(sub)
+mul = second_num * first_num
+console.log(mul)
+div = second_num / first_num
+console.log(div)
